1b6c02b6175f4403308e69748a27f685959a2439
CXY-YSL <63149562+CXY-YSL@users.noreply.github.com>
2020-04-15 00:47:31 +0800

    C#

---
+++ C#/登录注册/Form3.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace 登录注册
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(329, 161);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "点击此处返回登录界面";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("楷体", 42F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(110, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(598, 70);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "恭喜你登录成功！";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(399, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(329, 161);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "点击此处退出";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ C#/登录注册/登录.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using 登录;
+
+namespace 登录注册
+{
+    public partial class 登录 : Form
+    {
+        public 登录()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            this.textBox2.PasswordChar = '*';
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String userid, password;
+            userid = textBox1.Text;
+            password = textBox2.Text;
+            String ConnstrWindows = @"Data Source=LAPTOP-6HF7DH62;Initial Catalog=Demo;Integrated Security=True";
+            SqlConnection SqlCon = new SqlConnection(ConnstrWindows);
+            SqlCon.Open(); //打开数据库
+            String Sql = "select userid,password from [user] where userid='" + userid + "' and password='" + password + "'";
+            SqlCommand sqlCommand = new SqlCommand(Sql, SqlCon);
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            if (sqlDataReader.HasRows)
+            {
+                Form3 form3 = new Form3();
+                form3.Show();
+                this.Hide();
+            }
+            else
+            {
+                DialogResult dr = MessageBox.Show("是否注册新用户？", "登录失败", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+                    注册 form2 = new 注册();
+                    form2.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    this.Show();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            注册 form2 = new 注册();
+            form2.Show();
+            this.Hide();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+    }
+}
