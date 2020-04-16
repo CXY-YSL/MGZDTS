0046c5dc8eb56a6df6af8d2b9564cc2b6ad44e42
CXY-YSL <63149562+CXY-YSL@users.noreply.github.com>
2020-04-17 02:11:04 +0800

    aa

--- Java/character/src/skill/ShanXian.java
+++
@@ -1,9 +0,0 @@
-package skill;
-import strategy.SkillStrategy;
-public class ShanXian implements SkillStrategy{
-	@Override
-	public void useSkill() {
-		System.out.println("…¡œ÷£∫Œª“∆“ª∂Œæ‡¿Î£¨120√ÎCD°£");
-		}
-	}
-
